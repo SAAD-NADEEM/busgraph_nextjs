28323eb5eb91ffb126fb3ca7bddac06624990064
SAAD-NADEEM <saadngaming99090@gmail.com>
2026-09-15 22:08:00 +0500

    fix RouteLayer TypeScript errors

--- components/RouteLayer.tsx
+++ components/RouteLayer.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Source, Layer } from "react-map-gl/mapbox";
 
-export default function RouteLayer({ coordinates }) {
-  const geojson = {
-    type: "Feature",
+export default function RouteLayer({ coordinates }: { coordinates: [number, number][] }) {
+  const geojson: GeoJSON.Feature<GeoJSON.LineString> = {
+    type: "Feature" as const,
+    properties: {},
     geometry: {
-      type: "LineString",
+      type: "LineString" as const,
       coordinates,
     },
   };
